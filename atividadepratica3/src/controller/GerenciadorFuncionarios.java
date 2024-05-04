55af999c75cab10bf29e94ae08ccb463b0665eb0
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-05-04 20:32:37 -0300

    controller class might be finished

--- atividadepratica3/src/controller/GerenciadorFuncionarios.java
+++ atividadepratica3/src/controller/GerenciadorFuncionarios.java
@@ -1,5 +1,31 @@
 package controller;
 
-public class GerenciadorFuncionarios {
+import model.Funcionario;
+import java.util.ArrayList;
 
+public class GerenciadorFuncionarios {
+    private ArrayList<Funcionario> funcionarios = new ArrayList<>();
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+
+    public boolean removerFuncionario(String matricula) {
+        return funcionarios.removeIf(f -> f.getMatricula().equals(matricula));
+    }
+
+    public void listarFuncionarios() {
+        for (Funcionario f : funcionarios) {
+            System.out.println(f.getNome() + " - " + f.getMatricula());
+        }
+    }
+
+    public Funcionario buscarFuncionarioPorMatricula(String matricula) {
+        for (Funcionario f : funcionarios) {
+            if (f.getMatricula().equals(matricula)) {
+                return f;
+            }
+        }
+        return null;
+    }
 }
