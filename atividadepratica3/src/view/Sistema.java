eb1526013554a6ed48c3e5c6568b869d9da2f06e
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-05-04 20:33:36 -0300

    Starting to implement the system class / visualization

--- atividadepratica3/src/view/Sistema.java
+++ atividadepratica3/src/view/Sistema.java
@@ -1,5 +1,46 @@
 package view;
 
+import controller.GerenciadorFuncionarios;
+import model.Desenvolvedor;
+import model.Gerente;
+import model.Estagiario;
+
 public class Sistema {
-    
+    private static GerenciadorFuncionarios gerenciador = new GerenciadorFuncionarios();
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("1. Adicionar Funcionário");
+            System.out.println("2. Remover Funcionário");
+            System.out.println("3. Listar Funcionários");
+            System.out.println("4. Sair");
+            int opcao = Console.lerInt();
+
+            switch (opcao) {
+                case 1:
+                    adicionarFuncionario();
+                    break;
+                case 2:
+                    removerFuncionario();
+                    break;
+                case 3:
+                    listarFuncionarios();
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
+
+    private static void adicionarFuncionario() {
+        // Implementação de adicionar funcionário
+    }
+
+    private static void removerFuncionario() {
+        // Implementação de remover funcionário
+    }
+
+    private static void listarFuncionarios() {
+        gerenciador.listarFuncionarios();
+    }
 }
